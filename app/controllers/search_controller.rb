9db55debe08a419c01e0509bdcdabab78035dc0a
Nimesh Nikum <nimesh.nikum@gmail.com>
2010-01-10 10:59:10 +0530

    Created csv file from json data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,27 +6,19 @@ class SearchController < ApplicationController
   geocode_ip_address
 
   def index
-#    @geocode
-##    geocode = Geocoders::MultiGeocoder.geocode(request.remote_ip)
-#    p "1111111111111111111111111111111111"
-#    p geocode.lng
-#    p geocode.lat
     unless params[:q].blank?
       @search_results = JSON.parse(open("http://search.twitter.com/search.json?geocode=#{@geocode.lat},#{@geocode.lng},1000.0km&q=#{params[:q]}").read)["results"]
-      raise @search_results.inspect
-      @mycsv = FasterCSV.read("/home/nimesh/Desktop/geocsv/ajay.csv")
+
+#      @mycsv = FasterCSV.read("/home/nimesh/Desktop/geocsv/ajay.csv")
       csv_string = FasterCSV.generate do |csv|
-        @mycsv.each do |mycsv|
-          csv << mycsv
+        csv << ["id", "profile_image_url", "location", "text", "from_user", "to_user", "geo"]
+        @search_results.each do |result|
+          csv << [result["id"], result["profile_image_url"], result["location"], result["text"], result["from_user"], result["to_user"], result["geo"]]
         end
       end
       csv_file = File.open("my_new_csv.csv", 'w') {|f| f.write(csv_string) }
 
-      
-#      @search_results = "http://search.twitter.com/search.json?geocode=#{@geocode.lat},#{@geocode.lng},1000.0km&q=#{params[:q]}"
-#      raise @search_results.inspect
-#      a = @search_results.collect {|s| s["text"]}
-      b = exec("curl -i -X POST -u 'ajay123:regius' -F 'dataset[csv]=@my_new_csv.csv;name=my_new_csv.csv' http://geocommons.com/datasets.json")
+      dataset = exec("curl -i -X POST -u 'ajay123:regius' -F 'dataset[csv]=@my_new_csv.csv;name=my_new_csv.csv' http://geocommons.com/datasets.json")
 #
 #      @map_id = exec("curl -i -X GET -u 'ajay123:regius' http://geocommons.com/maps/new?overlay_id=#{69844}")
 #      raise c.inspect
