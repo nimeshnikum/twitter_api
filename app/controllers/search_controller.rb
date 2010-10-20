1330306834772c3d91b983533a90462e6c524c76
Nimesh Nikum <nimesh.nikum@gmail.com>
2010-10-20 12:34:58 +0530

    Created search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,16 @@
+require 'open-uri'
+require 'json'
+
 class SearchController < ApplicationController
   def index
-    unless params[:search_keyword].blank?
-      raise params[:search_keyword].inspect
+    unless params[:q].blank?
+      @search_results = JSON.parse(open("http://search.twitter.com/search.json?q=#{params[:q]}").read)['results']
+      a = @search_results.collect {|s| s["text"]}
+      raise a.inspect
+#      respond_to do |format|
+#        format.js {send(a)}
+#      end
+#      raise params[:q].inspect
     end
   end
 end
